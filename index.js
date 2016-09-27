974a60ff39f19355053dcb94a32a59e3ac88b4ed
always1122 <2079315083@qq.com>
2016-09-27 16:16:02 +0800

    mm

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+
+app.get('/:name', function (req, res) {
+  var username=req.params.name;
+  var page="<html>" +
+            "<body>" +
+            "<h1>" + username + "的购物车" + "</h1>" +
+            "</body>" +
+            "</html>"
+ res.send(page);
+})
+app.get('/hi', function (req, res) {
+  var page="<html>" +
+            "<body>" +
+            "<h1>index.html</h1>" +
+            "</body>" +
+            "</html>"
+ res.send(page);
+})
+app.listen(3000,function(){
+  console.log('running on port 3000...piz visit http://localhost:3000');
+})
